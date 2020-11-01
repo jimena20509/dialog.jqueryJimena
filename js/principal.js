6c4b7f0810de7c29ea79bd5d7acfbf1f77b08c65
Jimena <jjimenashirley@gmail.com>
2020-11-01 16:15:44 -0500

    Modificación 3

--- js/principal.js
+++ js/principal.js
@@ -1,18 +1,51 @@
 $(function(){
+
+
+    var celulares = [
+       'LG', 'Xiaomi', 'Huawei', 'iPhone' 
+    ]
+
+
+    
+    for (let i = 0; i < celulares.length; i++) {
+        const cell = celulares[i];
+        console.log(cell);
+        $('#lista-celulares').append(`
+            <li>
+            <span class="row">
+
+                <span class="col">
+                ${cell}
+                </span>
+                <span class="col">
+                    <a herf="a" class="btnEliminar">Eliminar</a>
+                </span>
+            </span>
+                
+            </li>
+        `);
+
+    }
+
+
     $('#dialogEliminar').dialog({
         autoOpen: false,
         modal: true,
         buttons: {
+           Acepto: function(){
+                alert('Eliminado') ;
+            },
             
-               'Acepto': function(){
-                  alert('Eliminar') 
-               },
-        },
-
+           Cancelar: function(){
+                console.log('Bye') ;
+            },
+          },
     });
 
 
-    $('.btnEliminar').on('click', function(){
-       $('#dialogEliminar').dialog('open') ;
+    $('#lista-celulares').on('click', '.btnEliminar', function (){
+        $("#dialogEliminar").dialog('open');
     })
+
+
 })
