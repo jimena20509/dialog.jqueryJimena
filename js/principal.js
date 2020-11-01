ba47c4af6ceabaeedbcd03c53e844aea93331601
Jimena <jjimenashirley@gmail.com>
2020-11-01 15:04:43 -0500

    Modificación 2

---
+++ js/principal.js
@@ -0,0 +1,17 @@
+$(function(){
+    $('#dialogEliminar').dialog({
+        autoOpen: false,
+        modal: true,
+        buttons: {
+            
+               'Acepto': function(){
+                  alert('Eliminar') 
+               },
+        },
+
+    });
+
+    $('.btnEliminar').on('click', function(){
+       $('#dialogEliminar').dialog('open') ;
+    })
+})
